32cb2e79fffe2c56f03f1bfda518ebab16bd22c1
Chakkrit Tunyong <chakkrit@livesync.io>
2018-03-21 11:27:37 +0700

    update view

--- template/app/controllers/example/index.js
+++ template/app/controllers/example/index.js
@@ -1,5 +1,5 @@
 module.exports = async(app) => {
     app.get('/example/', (req, res) => {
-        res.render('example/passport', { user: req.user });
+        res.render('example/main', { user: req.user });
     });
 };
